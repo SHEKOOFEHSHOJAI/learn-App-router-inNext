1cb7345283c1d23f79eddd87dd6d2ade38b84fa6
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2023-09-24 12:08:10 +0330

    start small project

---
+++ app-router-demo/app/events/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EventPage() {
+  return (
+    <p className="text-center">please select  an event</p>
+  )
+}
---
+++ app-router-demo/app/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main className=" p-24 max-w-6xl mx-auto flex bg-blue-700 flex-col items-center">
+      <h1 className="text-6xl font-bold text-center text-blue-50 mb-4">
+        Learn Build Teach
+      </h1>
+      <p className="text-white text-2xl text-center italic mb-10">
+        The safe community for developers to Learn, Build, and Teach
+      </p>
+      <Link href="/events" className="p-4 rounded-md bg-red-700  text-blue-950">
+        View Events
+      </Link>
+    </main>
+  );
+}
