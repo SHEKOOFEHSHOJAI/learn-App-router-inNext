b0c7cd99f61e97fe2c9ba5372ef8a763a4b89829
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2023-11-21 14:13:36 +0330

    end

--- todoList-typescript/todo/src/model/FullList.ts
+++ todoList-typescript/todo/src/model/FullList.ts
@@ -1,41 +1,53 @@
 import ListItem from "./ListItem";
 
+
 interface List {
     list: ListItem[],
     load(): void,
     save(): void,
     clearList(): void,
-    addItem(itemobj: ListItem): void,
+    addItem(itemObj: ListItem): void,
     removeItem(id: string): void,
 }
+
 export default class FullList implements List {
+
     static instance: FullList = new FullList()
+
     private constructor(private _list: ListItem[] = []) { }
+
     get list(): ListItem[] {
         return this._list
     }
+
     load(): void {
-        const storedList: string | null = localStorage.getItem("mylist")
+        const storedList: string | null = localStorage.getItem("myList")
         if (typeof storedList !== "string") return
+
         const parsedList: { _id: string, _item: string, _checked: boolean }[] = JSON.parse(storedList)
+
         parsedList.forEach(itemObj => {
-            const newList = new ListItem(itemObj._id, itemObj._item, itemObj._checked)
-            FullList.instance.addItem(newList)
+            const newListItem = new ListItem(itemObj._id, itemObj._item, itemObj._checked)
+            FullList.instance.addItem(newListItem)
         })
     }
+
     save(): void {
         localStorage.setItem("myList", JSON.stringify(this._list))
     }
+
     clearList(): void {
         this._list = []
         this.save()
     }
-    addItem(itemobj: ListItem): void {
-        this._list.push(itemobj)
+
+    addItem(itemObj: ListItem): void {
+        this._list.push(itemObj)
         this.save()
     }
+
     removeItem(id: string): void {
-        this._list = this._list.filter(item => item.id != id)
+        this._list = this._list.filter(item => item.id !== id)
         this.save()
     }
 }
--- todoList-typescript/todo/src/model/ListItem.ts
+++ todoList-typescript/todo/src/model/ListItem.ts
@@ -1,31 +1,38 @@
-export interface Item{
-    id:string,
-    item:string,
-    checked:boolean,
+export interface Item {
+    id: string,
+    item: string,
+    checked: boolean,
 }
-export default class ListItem implements Item{
+
+export default class ListItem implements Item {
+
     constructor(
-        private _id:string='',
-        private _item:string='',
-        private _checked:boolean=false,
-    ){  }
-  
-    get id():string{
+        private _id: string = '',
+        private _item: string = '',
+        private _checked: boolean = false,
+    ) { }
+
+    get id(): string {
         return this._id
     }
-    set id(id:string){
-        this._id=id;
+
+    set id(id: string) {
+        this._id = id
     }
-    get item():string{
+
+    get item(): string {
         return this._item
     }
-    set item(item:string){
-        this._item=item;
+
+    set item(item: string) {
+        this._item = item
     }
-    get checked():boolean{
+
+    get checked(): boolean {
         return this._checked
     }
-    set checked(checked:boolean){
-        this._checked=checked;
+
+    set checked(checked: boolean) {
+        this._checked = checked
     }
 }
